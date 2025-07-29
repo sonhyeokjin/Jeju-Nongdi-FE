045840c6c8473802323b386a48193fa13823c47f
namyeoungchan <namyeoungchan@users.noreply.github.com>
2025-07-29 00:39:22 +0000

    deploy: 18d4e93c30faa95b3c82ab37eed1475abc9a95bd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "209835604"
+    serviceWorkerVersion: "1854412476"
   }
 });
